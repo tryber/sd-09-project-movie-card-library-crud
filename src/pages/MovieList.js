3512863ec72ada2097a6f575ce9912329988ae79
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-03-03 16:52:17 -0300

    requirement 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -12,11 +13,17 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then(
+      (selectedMovies) => this.setState({ movies: selectedMovies }),
+    );
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    if (movies.length === 0) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
