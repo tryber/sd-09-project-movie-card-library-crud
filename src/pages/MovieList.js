a8f096ca018f9553176fe6c674fcc930e02c837e
Nathália <nathaliaveneziano@icloud.com>
2021-03-03 20:56:14 -0300

    Requisito 2 - Faça uma requisição para buscar e mostrar a lista de filmes quando `MovieList` for montado

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,17 +9,34 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  async fetchMovies() {
+    const movies = await movieAPI.getMovies();
+
+    this.setState({
+      movies,
+      loading: false,
+    })
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    const loadingElement = <div className="loading"> Carregando... </div>;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { (loading) ? loadingElement : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
