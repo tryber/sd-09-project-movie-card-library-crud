8d2929bd6e3e72b11c702549705bb2691906d925
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-03 15:41:13 -0300

    created structure of app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={ () => <MovieList /> } />
+        <Route path="/movies/:id" render={ () => <MovieDetails /> } />
+        <Route exact path="/movies/new" render={ () => <NewMovie /> } />
+        <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+        <Route render={ () => <NotFound /> } />
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
