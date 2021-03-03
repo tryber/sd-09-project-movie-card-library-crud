e8b7d27ddfc927c83a55004b99546ec5cec14ba9
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-03 18:22:17 -0300

    created structure of MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ movie.imagePath } alt="movieImage" />
+        <p>{ movie.title }</p>
+        <p>{ movie.subtitle }</p>
+        <p>{ movie.storyline }</p>
+        <p>{ movie.genre }</p>
+        <p>{ movie.rating }</p>
+        <Link to={ `movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
