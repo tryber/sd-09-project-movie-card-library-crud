f750967657363252c5218a4085f9f168c260f425
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-03 19:20:37 -0300

    Add fetchAPI in MovieList.js

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.fetchMovieAPI = this.fetchMovieAPI.bind(this);
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.fetchMovieAPI();
+  }
 
+  async fetchMovieAPI() {
+    const moviesList = await movieAPI.getMovies();
+    this.setState((previousState) => ({ movies:
+      [...previousState.movies, ...moviesList] }));
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { movies, loading } = this.state;
     // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      loading ? <Loading />
+        : (
+          <div data-testid="movie-list">
+            {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+          </div>
+        )
     );
   }
 }
