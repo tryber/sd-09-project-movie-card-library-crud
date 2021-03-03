304d67bf63ceec11fffa75b012ba0ccb73f33585
Renato Feitosa <pythaqua@gmail.com>
2021-03-03 19:45:48 -0300

    feat(MovieCard): add 3rd requirement

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,26 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-class MovieCard extends React.Component {
+export default class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id } } = this.props;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <p>{ title }</p>
+        <p>{ storyline }</p>
+        <p><Link to={ `/movies/${id}` }>VER DETALHES</Link></p>
       </div>
     );
   }
 }
 
-export default MovieCard;
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
+};
