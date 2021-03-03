5a404372eb85edc04fb2d6eb815f4f6ce4e53327
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-03-03 13:44:33 -0400

    Adiciona alteraçoes na requisiçao do requisito 4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NewMovie from './pages/NewMovie';
 import MovieDetails from './pages/MovieDetails';
 import MovieList from './pages/MovieList';
 import NotFound from './pages/NotFound';
+import Movies from './services/movieData';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Switch>
         <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
         <Route path="/movies/new" render={ () => <NewMovie /> } />
-        <Route path="/movies/:id" render={ () => <MovieDetails /> } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
         <Route exact path="/" render={ () => <MovieList /> } />
         <Route path="/" render={ () => <NotFound /> } />
       </Switch>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 
-import * as movieAPI from '../services/movieAPI';
+import { getMovie } from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+
+  constructor(props) {
+    super(props);
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const { match: { params } } = this.props;
+    try {
+      const movie = await getMovie(params.id);
+      this.setState({
+        movie,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    if (!this.state) {
+      return <Loading />;
+    }
+    const { movie: { title, storyline, imagePath, genre, rating, subtitle } } = this.state;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
