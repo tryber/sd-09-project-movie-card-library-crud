4622f2282cbf8854400bd0e15e85b06a04131549
LucioOSilva <lucioosilva00@gmail.com>
2021-03-03 16:13:31 -0300

    added Req1 e Req2

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './MovieCard.css';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    console.log(movie);
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div data-testid="movie-card" className="card-base">
+        <img src={ movie.imagePath } alt="card-img" className="card-image" />
+        <h1 className="card-title">{movie.title}</h1>
+        <p className="card-subtitle">{movie.storyline}</p>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = { movie: PropTypes.object }.isRequired;
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
+import './MovieList.css';
+
+const initialState = {
+  loading: true,
+  movies: [],
+};
 
 class MovieList extends Component {
   constructor() {
     super();
+    this.state = initialState;
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
 
-    this.state = {
-      movies: [],
-    };
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const requestReturn = await movieAPI.getMovies();
+    this.setState({
+      loading: false,
+      movies: requestReturn,
+    });
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
+    if (loading) return <Loading />;
     return (
-      <div data-testid="movie-list">
+      <div data-testid="movie-list" className="card-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {/* <MovieCard key={ movies.title } movie={ movies[0] } /> */}
       </div>
     );
   }
