cc52728571cc9a59187cecf0a390dd9e72603213
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-03-03 14:54:35 -0400

    Adiciona requisito 6

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
         <Route exact path="/movies/new" render={ () => <NewMovie /> } />
         <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
         <Route exact path="/" render={ () => <MovieList /> } />
