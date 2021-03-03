755f48f9a7efef2bae811944f1d4476593bdb5b0
Henrique Brito Elias <henriquebelias@gmail.com>
2021-03-03 16:03:48 -0300

    Requisito 3 - Insira um Link dentro de MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, storyline, imagePath } } = this.props;
     return (
       <div data-testid="movie-card">
         <img alt="Movie Cover" src={ imagePath } />
         <div>
           <h4>{title}</h4>
-          <h5>{subtitle}</h5>
           <p>{storyline}</p>
+        </div>
+        <div>
+          <Link to="/movies/:id">VER DETALHES</Link>
         </div>
       </div>
     );
@@ -20,7 +23,6 @@ class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
-    subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
   }).isRequired,
