b9cf89227e783a52b66916db5b225359f9331d55
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-03-03 16:38:51 -0300

    Requisito 6 adiciona pagina de criar filme

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Loading, MovieCard } from '../components';
 
 import * as movieAPI from '../services/movieAPI';
@@ -27,6 +28,7 @@ class MovieList extends Component {
 
     return (
       <div data-testid="movie-list">
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
