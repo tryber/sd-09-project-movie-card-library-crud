b759fd8cbe243abfd5d8fbdec8c9f3b8e4a4e8eb
santiago <felipesantiagolds@gmail.com>
2021-03-04 18:56:43 -0300

    1st requirement.Render BrowserRouter in the App component using routes. 2st requirement. Make a request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id/edit"><EditMovie /></Route>
+        <Route path="/movies/new"><NewMovie /></Route>
+        <Route path="/movies/:id"><MovieDetails /></Route>
+        <Route path="/"><MovieList /></Route>
+        <Route path="*"><p>NotFound </p></Route>
+      </Switch>
     </Router>
   );
 }
 
 export default App;
+
+/*
+- a rota `/` deve renderizar MovieList
+- a rota `/movies/:id` deve renderizar MovieDetails
+- a rota `/movies/new` deve renderizar NewMovie
+- a rota `/movies/:id/edit` deve renderizar EditMovie
+- qualquer rota não declarada deve renderizar NotFound
+*/
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -8,20 +8,43 @@ class MovieList extends Component {
     super();
 
     this.state = {
+      error: '',
+      isLoaded: false,
       movies: [],
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then(
+        (result) => this.setState({
+          isLoaded: true,
+          movies: result,
+        }),
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, isLoaded, error } = this.state;
 
     // Render Loading here if the request is still happening
-
-    return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
-    );
+    if (error) {
+      return <div>Error: { error.message }</div>;
+    } else if (!isLoaded) {
+      return <div>Carregando...</div>;
+    } else {
+      return (
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        </div>
+      );
+    }
   }
 }
 
