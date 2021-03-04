04beaca68acf570358c5df42cffa7af398257afc
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-03 22:25:02 -0300

    Add dynamic movie id route in Movie Card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -9,7 +9,7 @@ class MovieCard extends React.Component {
       <div data-testid="movie-card">
         <h1>{ movie.title }</h1>
         <p>{ movie.storyline }</p>
-        <Link to="movies/:id">VER DETALHES</Link>
+        <Link to={ `movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
