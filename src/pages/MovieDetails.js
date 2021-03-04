fd83b294af82a6fa0371b5e05ab51c742346c935
Raquel Picanço <picanco.raquel@gmail.com>
2021-03-04 09:46:02 -0400

    Create routing for MovieDetails and link to homepage

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      info: {},
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovieInfo();
+  }
+
+  async fetchMovieInfo() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const info = await movieAPI.getMovie(id)
+      .then((res) => res)
+      .catch((error) => {
+        console.error(error);
+        // Vou tratar o erro depois que atingir os 100% ^^'
+        return { error, message: 'Error fecthing movie info' };
+      });
+    this.setState({ info, loading: false });
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const { loading, info } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = info;
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
+      loading
+        ? <Loading />
+        : (
+          <div data-testid="movie-details">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            <p>{ `Title: ${title}` }</p>
+            <p>{ `Subtitle: ${subtitle}` }</p>
+            <p>{ `Rating: ${rating}` }</p>
+            <p>{ `Genre: ${genre}` }</p>
+            <p>{ `Storyline: ${storyline}` }</p>
+            <Link to="/">VOLTAR</Link>
+            <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          </div>
+        )
     );
   }
 }
