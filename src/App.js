70aa1438cd25e632cfe1c5e29b5134363ff6c37a
matheusjkl1 <matheus.unilog@gmail.com>
2021-03-04 02:40:00 -0300

    First commit :dollar:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
+    <Switch>
+      <Route path="/" render="" />
     <div>Movie Card Library CRUD</div>
+    </Switch>
   );
 }
 
