28918ec5c2150f0cbd3a88d9e8ac970428741fec
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-03 22:55:26 -0300

    Connect to API and get movie by ID in MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,19 +4,42 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: undefined,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getMovieFromAPI();
+  }
+
+  async getMovieFromAPI() {
+    const { id } = this.props.match.params;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({ movie });
+    this.setState({ loading: false });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie, loading } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
       </div>
     );
   }
