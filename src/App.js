6a31ef40efc77ad42eaa47335db5514ce0bad097
Hudson Carlos <hc88806@gmail.com>
2021-03-04 16:46:58 -0400

    add route to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NotFound from './pages/NotFound';
+import MovieDetails from './pages/MovieDetails';
+import NewMovies from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
-    <div>Hudson Carlos Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route exact path='/' component={MovieList} />
+      <Route path='/movies/:id' component={MovieDetails} />
+      <Route path='/movies/new' component={NewMovies} />
+      <Route path='/movies/:id/edit' component={EditMovie} />
+      <Route component={NotFound} />
+    </BrowserRouter>
   );
 }
 
