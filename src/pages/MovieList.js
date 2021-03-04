ac232ec0102fbc2b193f4c2eb1720af1f19fcd17
jiarguello <jhonatan.arguello@gmail.com>
2021-03-04 16:20:46 -0300

    Inserindo component loading na tela enquanto nao ha o retorno da API para a renderizaçao da MovieList.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 import { Route } from 'react-router-dom';
-
 import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieList extends Component {
   constructor() {
@@ -13,11 +13,12 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then(response => this.setState({ movies: response }));
+  }
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    if(movies.length === 0) return <Loading />
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
