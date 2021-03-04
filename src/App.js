20553f20a957a65c3ce5614e7ce76f2772759442
ViniciusChagas <viniciushrcs@gmail.com>
2021-03-04 16:29:17 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Route exact path="/" component={ MovieList } />
+      <Route exact path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route component={ NotFound } />
+    </BrowserRouter>
   );
 }
 
