9464c40d3d901c2785e210b8f82644e3d9116cdc
Raquel Picanço <picanco.raquel@gmail.com>
2021-03-03 20:30:10 -0400

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-    //
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>Movie Card Library CRUD</div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ MovieList } />
+            <Route path="/movies/new" component={ NewMovie } />
+            <Route exact path="/movies/:id" component={ MovieDetails } />
+            <Route path="/movies/:id/edit" component={ EditMovie } />
+            <Route component={ NotFound } />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
