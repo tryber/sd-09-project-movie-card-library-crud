ed9b229c07ee9a38b8ef02d6c318dbcb1902a681
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-03 22:20:07 -0300

    Change Swith in App.js and Add Typechecking in MovieCard

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import MovieList from './pages/MovieList';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/" component={ MovieList } />
+        <Route exact path="/" component={ MovieList } />
+        <Route component={ NotFound } />
       </Switch>
     </Router>
   );
