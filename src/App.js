244f17e7ee547c48402cd767f4c512f2d42e12e5
Matheus Martino <matheus.martino97@gmail.com>
2021-03-04 11:32:08 -0300

    Adding loading message through props.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index'
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <Switch>
         <Route exact path="/" render={ () => <MovieList /> } />
         <Route exact path="/movies/:id" render={ () => <MovieDetails /> } />
-        <Route id="new-movie" data-testid="new-movie" exact path="/movies/new" render={ () => <NewMovie /> } />
+        <Route exact path="/movies/new" render={ () => <NewMovie /> } />
         <Route exact path="/movies/:id/edit" render={ () => <EditMovie /> } />
         <Route path="*" render={ () => <NotFound /> } />
       </Switch>
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class Loading extends Component {
   render() {
-    return (
-      <div>{ this.props.loadingMessage }</div>
-    );
+    const { loadingMessage } = this.props;
+
+    return <div>{loadingMessage}</div>;
   }
 }
+
+Loading.propTypes = {
+  loadingMessage: PropTypes.string.isRequired,
+};
 
 export default Loading;
