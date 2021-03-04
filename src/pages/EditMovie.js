7596778ab563542ac550d66fc45007eff4e6d341
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-04 17:40:47 -0300

    Add EditMovie functionality

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: [],
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.getMovieFromAPI();
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  async getMovieFromAPI() {
+    const { id } = this.props.match.params;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({ movie });
+    this.setState({ status: 'complete' });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return (
+        <Redirect to="/" />
+      );
     }
 
     if (status === 'loading') {
-      // render Loading
+      return (
+        <Loading />
+      );
     }
 
     return (
