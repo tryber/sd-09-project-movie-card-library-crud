da9eaad989627e98644397b5b93be9434ac4e687
matheusjkl1 <matheus.unilog@gmail.com>
2021-03-04 05:52:19 -0300

    lint cc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Link, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import MovieList from './pages/MovieList'
 import NewMovie from './pages/NewMovie'
 import MovieDetails from './pages/MovieDetails'
