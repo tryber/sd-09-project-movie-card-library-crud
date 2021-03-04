f1d2b00e45262e70bdf8a3ff8bc5fbc7fc0733cc
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-04 16:09:31 -0300

    Initi this.state.movie as Array instead undefined

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -8,7 +8,7 @@ class MovieDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: undefined,
+      movie: [],
       loading: true,
     };
   }
@@ -27,21 +27,21 @@ class MovieDetails extends Component {
   render() {
     const { movie, loading } = this.state;
     const { id } = this.props.match.params;
-    if (loading) {
-      return <Loading />;
-    }
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{`Title: ${title}`}</p>
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
-        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
-        <Link to="/">VOLTAR</Link>
-      </div>
+      loading ? <Loading />
+        : (
+          <div data-testid="movie-details">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            <p>{`Title: ${title}`}</p>
+            <p>{`Subtitle: ${subtitle}`}</p>
+            <p>{`Storyline: ${storyline}`}</p>
+            <p>{`Genre: ${genre}`}</p>
+            <p>{`Rating: ${rating}`}</p>
+            <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+            <Link to="/">VOLTAR</Link>
+          </div>
+        )
     );
   }
 }
