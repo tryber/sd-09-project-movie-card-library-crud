cca90aaf4f58a4e0bd691dee8da9f52e12c01bd1
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-03-04 18:26:54 -0300

    corrigindo issues do lint

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -37,7 +37,7 @@ class MovieDetails extends Component {
         <p>{ `Rating: ${rating}` }</p>
         <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
         <Link to="/">VOLTAR</Link>
-        <Link to="/" onClick={ ()=> movieAPI.deleteMovie(id) }>DELETAR</Link>
+        <Link to="/" onClick={ () => movieAPI.deleteMovie(id) }>DELETAR</Link>
       </div>
     );
   }
