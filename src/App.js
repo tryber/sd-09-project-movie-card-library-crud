94986f45499f83e36c06d2022ac02cb68386ac0d
edison-ps <edison@osite.com.br>
2021-03-04 19:43:51 -0300

    App V1.00

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Movelist from './pages/MovieList'
+import MovieDetails from './pages/MovieDetails';
+import NemMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>
+        <p>Movie Card Library CRUD</p>
+        <Route path="/" component={ Movelist } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NemMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
