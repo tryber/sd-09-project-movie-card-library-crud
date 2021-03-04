4ac4ffda260a2777183779af477df9fbf0eb4367
Felipe Cardoso <felipedclc@gmail.com>
2021-03-04 15:54:09 -0300

    requisito 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -16,6 +16,7 @@ function App() {
         <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route component={ NotFound } />
       </Switch>
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
     </BrowserRouter>
   );
 }
