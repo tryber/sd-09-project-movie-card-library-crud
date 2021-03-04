bd81fb4ca4e80a1282ff024366013160b5ed1c26
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-04 19:08:54 -0300

    Correct typecheck

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,17 @@ class MovieForm extends React.Component {
   }
 }
 
+MovieForm.propTypes = {
+  movie: PropTypes.objectShape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  }).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 export default MovieForm;
