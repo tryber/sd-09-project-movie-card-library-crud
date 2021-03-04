d879598c07c8bc514efaf24ba938c71910f70cba
matheusjkl1 <matheus.unilog@gmail.com>
2021-03-04 06:31:41 -0300

    lint cc v11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import MovieList from './pages/MovieList'
-import NewMovie from './pages/NewMovie'
-import MovieDetails from './pages/MovieDetails'
-import EditMovie from './pages/EditMovie'
-import NotFound from './pages/NotFound'
-import './App.css'
-
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter >
-    <Switch>
-      <Route exact path="/" component={MovieList} />
-      <Route exact path="/movies/:id" component={MovieDetails} />
-      <Route exact path="/new" component={NewMovie} />
-      <Route exact path="/movies/:id/edit" component={EditMovie} />
-      <Route component={NotFound} />
-    </Switch>
+      <Switch >
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
