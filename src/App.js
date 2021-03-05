90e1fc13813d8999903f7ea11de65053ea85c150
Luciano_Lodi <lrlodi@gmail.com>
2021-03-05 10:33:42 -0300

    Requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ MovieList } />
-        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/new" render={ (props) => <NewMovie { ...props } /> } />
         <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
         <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
         <Route component={ NotFound } />
