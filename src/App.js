bea72c4164fdb0dcae06345826c7405d88afd323
lcgoandete <lcgoandete@hotmail.com>
2021-03-05 20:30:23 -0300

    5 - Realize uma requisição para buscar o filme que será editado em `EditMovie`

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MovieDetails from './pages/MovieDetails';
-import MovieList from './pages/MovieList';
-import EditMovie from './pages/EditMovie';
-import NewMovie from './pages/NewMovie';
-import NotFound from './pages/NotFound';
+import { MovieDetails, MovieList, EditMovie, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
@@ -13,7 +9,10 @@ function App() {
         <Switch>
           <Route exact path="/" component={ MovieList } />
           <Route path="/movies/new" component={ NewMovie } />
-          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route
+            path="/movies/:id/edit"
+            render={ (props) => <EditMovie { ...props } /> }
+          />
           <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
           <Route component={ NotFound } />
         </Switch>
