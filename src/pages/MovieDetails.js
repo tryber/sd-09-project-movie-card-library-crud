1cb1f8a5f3ce6785ea90b406c856526671d509b0
brenno-calado <brenno.calado@gmail.com>
2021-03-05 10:08:34 -0300

    fix eslint errors

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -57,7 +57,7 @@ class MovieDetails extends Component {
 MovieDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
     }),
   }).isRequired,
 };
