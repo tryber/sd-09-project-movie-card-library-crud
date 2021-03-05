e9b4d612944fc9ae2370581cdd07a0f3245f8805
ViniciusChagas <viniciushrcs@gmail.com>
2021-03-04 21:58:57 -0300

    corrigindo issue do PropTypes

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
