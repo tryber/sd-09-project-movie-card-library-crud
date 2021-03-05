ba2cdaa4f092969076586aa52c3dfc9535664f0b
ViniciusChagas <viniciushrcs@gmail.com>
2021-03-04 21:53:18 -0300

    corrigindo issue

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
 
+import { Link } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
-import { Link } from 'react-router-dom';
 
 class MovieList extends Component {
   constructor(props) {
