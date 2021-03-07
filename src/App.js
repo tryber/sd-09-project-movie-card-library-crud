abfda4c550239ad93754123a543134ce41cb4c2b
marcusWittho <witthoeft.mcs@hotmail.com>
2021-03-07 20:46:11 -0300

    Resolução do requisito 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <MovieList />
-        <MovieDetails />
-        <NewMovie />
-        <EditMovie />
-        <NotFound />
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route
+          exact
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { imagePath, title, storyline } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
+        <h4>{ title }</h4>
+        <p>{ storyline }</p>
       </div>
     );
   }
