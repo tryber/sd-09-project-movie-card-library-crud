ec67bbc69f51ed705ee0b8536d570f6c8c0e7e0a
Pedro Henrique <pedroenrique.400@gmail.com>
2021-03-07 14:00:50 -0300

    Corrige requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -13,9 +13,7 @@ class MovieCard extends React.Component {
           <h4 data-testid="movie-card-title" className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
-          <Link to={ `/movies/${id}` }>
-            Ver Detalhes
-          </Link>
+          <Link to={ `/movies/${id}` }>Ver Detalhes</Link>
         </div>
       </div>
     );
