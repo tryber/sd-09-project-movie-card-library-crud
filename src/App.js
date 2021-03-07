0aa4f1c551ff4cb04882b59d40a2b515be7900cf
Thiago Granville <thiagogranville@gmail.com>
2021-03-07 16:47:33 -0300

    Requisito 1: Correções na importação do react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -12,11 +12,11 @@ function App() {
     <Router>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <route exact path="/" component={ MovieList } />
-        <route path="/movies/:id" component={ MovieDetails }/>
-        <route path="/movies/new" component={ NewMovie } />
-        <route path="/movies/:id/edit" component={ EditMovie } />
-        <route component={ NotFound } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails }/>
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
       </Switch>
     </Router>
   );
