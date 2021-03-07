2b453e752054c2346fd16ad73819b5e6a84db762
Pedro Henrique <pedroenrique.400@gmail.com>
2021-03-07 13:44:43 -0300

    Corrige requisito 2/eslint/8

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -54,7 +54,7 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
-        <Link to='/'>VOLTAR</Link>
+        <Link to="/">VOLTAR</Link>
 
       </div>
     );
