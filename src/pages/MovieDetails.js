0c16c6b99b7bcc5d070c6eba0143ef3cc6f92799
Pedro Henrique <pedroenrique.400@gmail.com>
2021-03-07 12:31:20 -0300

    Tenta completar o requisto 2 e 3

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
 
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
 
-    // const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      loading: true,
+      id: this.props.match.params.id
 
-    const { storyline, imagePath, genre, rating, subtitle } = {};
+    }
+    this.showMovie = this.showMovie.bind(this);
+  }
+
+  async fetchMovie() {
+    this.setState(
+      { loading: true },
+
+      async () => {
+        let { id } = this.state;
+        const requestMovie = await movieAPI.getMovie(id);
+        this.setState({
+          movie: requestMovie,
+          loading: false,
+    
+        });
+      }
+    );
+  }
+  componentDidMount() {
+    this.fetchMovie()
+  }
+
+  showMovie() {
+    const { movie } = this.state;
+    const { storyline, imagePath, genre, rating, subtitle } = movie ;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
+      <img alt="Movie Cover" src={ `../${ imagePath }` } />
+      <p>{ `Subtitle: ${ subtitle }` }</p>
+      <p>{ `Storyline: ${ storyline }` }</p>
+      <p>{ `Genre: ${ genre }` }</p>
+      <p>{ `Rating: ${ rating }` }</p>
+    </div>
     );
+  }
+
+  render() {
+    // Change the condition to check the state
+    // if (true) return <Loading />;
+    const { loading } = this.state;
+    return (loading ? <Loading /> : this.showMovie() );
   }
 }
 
