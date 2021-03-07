627657d9a25000a5dc4c215360bf3feee93af2be
marcusWittho <witthoeft.mcs@hotmail.com>
2021-03-07 00:54:41 -0300

    Resolução do requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <MovieList />
+        <MovieDetails />
+        <NewMovie />
+        <EditMovie />
+        <NotFound />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
