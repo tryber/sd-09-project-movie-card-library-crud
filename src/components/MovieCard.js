81c5e09fad4127f37db2ab411ad951effb96a1d5
Pedro Henrique <pedroenrique.400@gmail.com>
2021-03-08 12:11:48 -0300

    conserta o requisito 6

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -13,7 +13,7 @@ class MovieCard extends React.Component {
           <h4 data-testid="movie-card-title" className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
-          <button type="button"><Link to={ `/movies/${id}` }>VER DETALHES</Link></button>
+          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
         </div>
       </div>
     );
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
@@ -41,6 +42,7 @@ class MovieList extends Component {
       <div className="movies">
         Movie Card Library CRUD
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
