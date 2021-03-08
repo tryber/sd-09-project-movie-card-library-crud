28529651831c5d05a8b66b51a73598332353f5c2
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-03-08 13:59:03 -0300

    Start requirements 5 and 6

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      movie: {},
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    return movieAPI.getMovie(id).then((receivedMovie) => {
+      this.setState({
+        status: 'loaded',
+        movie: receivedMovie,
+      });
+    });
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie).then(() => {
+      this.setState({
+        shouldRedirect: true,
+      });
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
-
-    if (status === 'loading') {
-      // render Loading
-    }
+    if (shouldRedirect) return <Redirect to="/" />;
+    if (status === 'loading') return <Loading />;
 
     return (
       <div data-testid="edit-movie">
@@ -30,5 +45,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      movie: {
+        title: '',
+        subtitle: '',
+        storyline: '',
+        rating: 0,
+        imagePath: '',
+        genre: 'undefined',
+      },
+      shouldRedirect: false,
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
-    movieAPI.updateMovie(newMovie);
+    this.setState({
+      shouldRedirect: true,
+    })
+    return movieAPI.createMovie(newMovie);
   }
 
   render() {
+    const { shouldRedirect, movie } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
+        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
