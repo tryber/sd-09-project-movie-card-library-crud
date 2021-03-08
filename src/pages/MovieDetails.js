7825229a2fb12bb5fef9ac198365207fc26eca45
Thiago Granville <thiagogranville@gmail.com>
2021-03-08 02:36:29 -0300

    Correções para o lint

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import propTypes from 'prop-types';
 
 class MovieDetails extends Component {
   constructor() {
@@ -29,17 +29,17 @@ class MovieDetails extends Component {
     const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
-      loading ? <Loading /> 
-      : (<div data-testid="movie-details">
-          <img alt="Movie Cover" src={ `../${imagePath}` } />
-          <h1>{ `Title: ${title}` }</h1>
-          <p>{ `Subtitle: ${subtitle}` }</p>
-          <p>{ `Storyline: ${storyline}` }</p>
-          <p>{ `Genre: ${genre}` }</p>
-          <p>{ `Rating: ${rating}` }</p>
-          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
-          <Link to="/">VOLTAR</Link>
-         </div>)
+      loading ? <Loading />
+        : (<div data-testid="movie-details">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            <h1>{ `Title: ${title}` }</h1>
+            <p>{ `Subtitle: ${subtitle}` }</p>
+            <p>{ `Storyline: ${storyline}` }</p>
+            <p>{ `Genre: ${genre}` }</p>
+            <p>{ `Rating: ${rating}` }</p>
+            <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+            <Link to="/">VOLTAR</Link>
+        </div>)
     );
   }
 }
@@ -50,7 +50,6 @@ MovieDetails.propTypes = {
       id: propTypes.number,
     }),
   }),
-}.isRequired
-
+}.isRequired;
 
 export default MovieDetails;
