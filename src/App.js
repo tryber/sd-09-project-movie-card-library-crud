3d69fba28fd06dc00f0b9489a00b858118b184b1
Matheus Martino <matheus.martino97@gmail.com>
2021-03-08 09:57:42 -0300

    Solving lint issues.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ MovieList } />
-        <Route exact path="/movies/new" component={ NewMovie } />
-        <Route exact path="/movies/:id/edit" component={ EditMovie } />
-        <Route exact path="/movies/:id" render={ (props) => <MovieDetails {...props} /> } />
-        <Route path="" component={ NotFound } />
+        <Route exact path='/' component={MovieList} />
+        <Route exact path='/movies/new' component={NewMovie} />
+        <Route exact path='/movies/:id/edit' component={EditMovie} />
+        <Route
+          exact
+          path='/movies/:id'
+          render={(props) => <MovieDetails { ...props } />}
+        />
+        <Route path='' component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'
 
 class MovieCard extends React.Component {
   render() {
+    const { title, storyline, id } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        <h1>{ this.props.movie.title }</h1>
-        <h2>{ this.props.movie.storyline }</h2>
-        <Link to={`/movies/${this.props.movie.id}`}>VER DETALHES</Link>
+        <h1>{ title }</h1>
+        <h2>{ storyline }</h2>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+    storyline: PropTypes.string,
+  }),
+}.isRequired;
 
 export default MovieCard;
