86f6057df903f285a31615537efbda3e33971b99
Pedro Henrique <pedro.876.ph@gmail.com>
2021-03-08 13:52:15 -0300

    acertos eslint req 1

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <div>Movie Card Library CRUD</div>
       <BrowserRouter>
         <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route component={ NotFound } />
       </BrowserRouter>
     </div>
