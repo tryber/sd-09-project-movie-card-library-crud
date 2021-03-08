ff2d75ce8a7c83cffeb9c89d243289670b2359cf
Pedro Henrique <pedro.876.ph@gmail.com>
2021-03-08 11:31:40 -0300

    requisito 2 e 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
+// conteudo do render e suas devidas prop-types foram copiadas do meu PR referente ao projeto do bloco 11
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, subtitle, storyline, id, imagePath } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt="" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    id: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -12,11 +12,15 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then((value) => this.setState({ movies: value }));
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    if (movies.length < 1) return <Loading />;
+    if (movies.length > 0) console.log(movies);
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
