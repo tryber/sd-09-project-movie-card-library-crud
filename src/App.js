8bdcf5d5b8687774adb2b6e004f1f0b656e50853
Marina Alane <marina.alane@outlook.com>
2021-03-08 10:30:57 -0300

    cria rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD :D </div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <section>
+        <Route path="/" Component={ MovieList } />
+        <Route path="/movies/:id" Component={ MovieDetails } />
+        <Route path="/movies/new" Component={ NewMovie } />
+        <Route path="/movies/:id/edit" Component={ EditMovie } />
+        <Route path="/" Component={ MovieList } />
+        <Route Component={ NotFound } />
+      </section>
+    </BrowserRouter>
   );
 }
 
