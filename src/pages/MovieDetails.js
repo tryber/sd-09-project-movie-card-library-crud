9c0634762bf5098333531d518b9a53d3e1735687
Thiago Granville <thiagogranville@gmail.com>
2021-03-08 02:22:07 -0300

    Requisito 4: Faz requisição para renderizar MovieDetails com base no filme clicado

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const { match } = this.props;
+    const movieObject = await movieAPI.getMovie(match.params.id);
+    this.setState({ movie: movieObject, loading: false});
+  }
+
+  render() {
+    const { loading, movie } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
-      <div data-testid="movie-details">
+      loading ? <Loading /> :
+      (<div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h1>{ `Title: ${title}` }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
-      </div>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to={ '/' }>VOLTAR</Link>
+      </div>)
     );
   }
 }
