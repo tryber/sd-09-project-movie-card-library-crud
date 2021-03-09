f6e7944fdfd4b8913f0860141d5d1570192df81b
Enio Nicael <enionicael@gmail.com>
2021-03-09 00:27:49 -0300

    Requisito 1, Renderiza BrowserRouter no componente App usando rotas.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import MovieList from './pages/MovieList'
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      {/* <div>Movie Card Library CRUD</div> */}
+      <Route path="/" component={MovieList} />
     </Router>
   );
 }
