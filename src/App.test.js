30a8e6944a3f76abd0e6097ce8e111e29ffe0647
Felipe Rocha <felipe.sr27@gmail.com>
2021-03-09 12:42:18 -0300

    Requirement 4 and css

--- src/App.test.js
+++ src/App.test.js
@@ -164,7 +164,7 @@ describe('3 - Insira um link para a página de detalhes de um filme dentro de `M
 
 describe('4 - Faça uma requisição para buscar o filme que deverá ser renderizado dentro de `Movie Details`', () => {
 
-  it.skip('deverá exibir o texto "Carregando..." enquanto estiver fazendo a requisição', async () => {
+  it('deverá exibir o texto "Carregando..." enquanto estiver fazendo a requisição', async () => {
     for (const movie of readMovies()) {
       await cleanup();
       const { getByText } = renderPath('/movies/' + movie.id);
@@ -172,7 +172,7 @@ describe('4 - Faça uma requisição para buscar o filme que deverá ser renderi
     }
   });
 
-  it.skip('deverá exibir o título, o subtítulo, a sinopse, a imagem e o gênero do filme', async () => {
+  it('deverá exibir o título, o subtítulo, a sinopse, a imagem e o gênero do filme', async () => {
     for (const movie of readMovies()) {
       const { container, unmount } = renderPath(`/movies/${movie.id}`);
       await waitFor(() => movieAPI.getMovie(movie.id))
@@ -181,14 +181,14 @@ describe('4 - Faça uma requisição para buscar o filme que deverá ser renderi
         .toBeGreaterThanOrEqual(1);
       expect(screen.getByText(readMovies()[movie.id - 1].storyline, { exact: false })).toBeTruthy;
 
-      let image = screen.getByAltText('Movie Cover').src.split.skip('/').slice(-2).join('/')
+      let image = screen.getByAltText('Movie Cover').src.split('/').slice(-2).join('/')
       expect(image).toEqual(readMovies()[movie.id - 1].imagePath);
       expect(screen.getAllByText(readMovies()[movie.id - 1].genre, { exact: false })).toBeTruthy;
       unmount();
     }
   });
 
-  it.skip('deverá conter um botão com o texto "VOLTAR" que redireciona para a página inicial', async () => {
+  it('deverá conter um botão com o texto "VOLTAR" que redireciona para a página inicial', async () => {
     for (const movie of readMovies()) {
       const { container, unmount, findByText } = renderPath('/movies/' + movie.id);
       await waitFor(() => movieAPI.getMovie(movie.id));
@@ -198,7 +198,7 @@ describe('4 - Faça uma requisição para buscar o filme que deverá ser renderi
     }
   });
 
-  it.skip('deverá conter um botão com o texto "EDITAR" que redireciona para a página de edição de filme', async () => {
+  it('deverá conter um botão com o texto "EDITAR" que redireciona para a página de edição de filme', async () => {
     for (const movie of readMovies()) {
       const { container, unmount, findByText } = renderPath('/movies/' + movie.id);
       await waitFor(() => movieAPI.getMovie(movie.id));
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
+import '../styles/Pages/MovieDetails.css';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      loadding: true,
+    };
+    this.getMovie = this.getMovie.bind(this);
+    this.loadParagraph = this.loadParagraph.bind(this);
+  }
 
-    const { /* title, */ storyline, imagePath, genre, rating, subtitle } = {};
+  async componentDidMount() {
+    const { match } = this.props;
+    const movie = await movieAPI.getMovie(match.params.id);
+    this.getMovie(movie);
+  }
 
+  getMovie(movie) {
+    this.setState({
+      movie,
+      loadding: false,
+    });
+  }
+
+  loadParagraph({ title, subtitle, storyline, genre, rating }) {
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
+      <>
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+      </>
+    );
+  }
+
+  render() {
+    const { loadding, movie } = this.state;
+    const { id, imagePath } = movie;
+    return (
+      <div className="movie-details-container" data-testid="movie-details">
+        { (loadding) ? (<Loading />) : (
+          <div className="movie-details-card">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            { this.loadParagraph(movie) }
+            <div>
+              <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+              <Link to="/">VOLTAR</Link>
+            </div>
+          </div>
+        ) }
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
