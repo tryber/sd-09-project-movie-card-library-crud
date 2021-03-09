20e9714c4e6d2f0e49e203b992a756e0ba0765e4
palenske <viniciuspalenske@gmail.com>
2021-03-08 23:03:26 -0300

    [FIX][defined but never used]

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-import * as movieAPI from '../services/movieAPI';
+// import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
