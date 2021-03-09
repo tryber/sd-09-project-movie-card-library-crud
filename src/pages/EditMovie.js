602a73dee978e3a10f24f45d43b4fb930d35ac67
Nícolas Tozato <ntozato9@gmail.com>
2021-03-09 10:49:42 -0300

    fix lint

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -51,5 +52,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default EditMovie;
