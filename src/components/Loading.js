e0b0af38c24bffc8a7e6a2b7a7dd3370109912dd
Felipe Rocha <felipe.sr27@gmail.com>
2021-03-09 10:30:31 -0300

    Requirement 3 and css

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import '../styles/Components/Loading.css';
 
 class Loading extends Component {
   render() {
     return (
-      <div>Carregando...</div>
+      <div className="loading-container">Carregando...</div>
     );
   }
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
+import '../styles/Pages/MovieList.css';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -30,7 +31,7 @@ class MovieList extends Component {
   render() {
     const { movies, loading } = this.state;
     return (
-      <div data-testid="movie-list">
+      <div className="movie-list-container" data-testid="movie-list">
         { (loading) ? (
           <Loading />
         ) : (
