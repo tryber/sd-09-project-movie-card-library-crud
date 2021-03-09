304a239f5b28e1894e243246bd8d7abdcde8650a
cleber-teixeira <cleberlt@gmail.com>
2021-03-09 07:57:56 -0300

    Requisito 03 - Inserir um link para a página de detalhes de um filme dentro de "MovieCard". :white_check_mark:

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+// import MovieDetails from '../pages/MovieDetails';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, imagePath } = movie;
+    const { id, title, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
@@ -12,6 +14,7 @@ class MovieCard extends React.Component {
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
+        <Link to={ `/movies/${ id }` }>VER DETALHES</Link>
       </div>
     );
   }
