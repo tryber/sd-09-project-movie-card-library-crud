a3d97a348a48d2dc9a461cb8ebe7adc4a2905726
Nícolas Tozato <ntozato9@gmail.com>
2021-03-09 13:08:03 -0300

    lint

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+// import MovieList from '../pages/MovieList';
+import PropTypes from 'prop-types';
 
 class MovieForm extends React.Component {
   constructor(props) {
@@ -163,5 +164,19 @@ class MovieForm extends React.Component {
     );
   }
 }
+
+MovieForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieForm;
