5245b44318f344fef7d5d90688b11d9857389bac
jiarguello <jhonatan.arguello@gmail.com>
2021-03-08 22:26:27 -0300

    Atualizando a renderizaçao da pagina inicial.

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { newMovie: false };
   }
 
-  // handleSubmit(newMovie) {
-  // }
+  handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie);
+    this.setState({ newMovie: true})
+  }
+
+  updateState(field, newValue) {
+    this.setState({ [field]: newValue });
+  }
 
   render() {
+    const { newMovie } = this.state;
+    if (newMovie) {
+      return <Redirect to="/" />;
+    }
     return (
-      <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
-      </div>
-    );
+        <div data-testid="new-movie">
+          <MovieForm onSubmit={ this.handleSubmit } />
+        </div>
+      );
   }
 }
 export default NewMovie;
