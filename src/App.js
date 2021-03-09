b514e3b9ee93afff927f24ab12c458f3c38d030b
cleber-teixeira <cleberlt@gmail.com>
2021-03-09 06:11:54 -0300

    Requisito 02 - Refatoração. :white_check_mark:

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
+      {/* <div>Movie Card Library CRUD</div> */}
       <Switch>
         <Route exact path="/" component={ MovieList } />
-        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/movies/:id/edit" component={ EditMovie } />
         <Route component={ NotFound } />
       </Switch>
