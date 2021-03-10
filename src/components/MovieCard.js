a24a28f6c0f6293e372a727304968dcb2dc889c0
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-03-10 12:21:33 -0300

    requirement 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, imagePath } = movie;
+    const { id, title, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
         <img src={ `../${imagePath}` } alt="" />
         <h1>{ title }</h1>
         <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
@@ -17,6 +19,7 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
@@ -7,8 +7,9 @@ class MovieDetails extends Component {
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
+    const { match: { params: { id } } } = this.props;
     const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    console.log(id);
     console.log(movieAPI);
     console.log(Loading);
     console.log(title);
@@ -24,5 +25,13 @@ class MovieDetails extends Component {
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
