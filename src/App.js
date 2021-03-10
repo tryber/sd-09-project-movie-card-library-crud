90b1a29ecd3358d2ee92527b57d2a48ce1d66511
Thiago Granville <thiagogranville@gmail.com>
2021-03-10 17:34:22 -0300

    Refatora importações no App usando o Index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
-import NewMovie from './pages/NewMovie';
-import EditMovie from './pages/EditMovie';
-import NotFound from './pages/NotFound';
+import * as index from './pages/index';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={ MovieList } />
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route component={ NotFound } />
-      </Switch>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ index.MovieList } />
+          <Route path="/movies/new" component={ index.NewMovie } />
+          <Route path="/movies/:id/edit" component={ index.EditMovie } />
+          <Route path="/movies/:id" component={ index.MovieDetails } />
+          <Route component={ index.NotFound } />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
