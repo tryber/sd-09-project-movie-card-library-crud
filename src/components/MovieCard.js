f5846f128c4cbc80db9ecdcec10ff29b25051dcf
Rodrigo Farias <deltadigo@gmail.com>
2021-03-10 18:53:49 -0300

    Implementação dos requisitos 2, 3 e 4

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropType from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, storyline } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{ title }</h1>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropType.shape({
+    id: PropType.number.isRequired,
+    title: PropType.string.isRequired,
+    subtitle: PropType.string.isRequired,
+    storyline: PropType.string.isRequired,
+    rating: PropType.number.isRequired,
+    imagePath: PropType.string.isRequired,
+    bookmarked: PropType.bool.isRequired,
+    genre: PropType.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -12,11 +13,24 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then((resolve) => {
+      this.setState({
+        movies: resolve,
+      });
+    });
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
+    if (movies.length <= 0) {
+      return (
+        <div data-testid="movie-list">
+          <Loading />
+        </div>
+      );
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
