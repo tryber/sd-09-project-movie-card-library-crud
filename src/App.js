f3257b1b9373a6a2dd4979884ea0e6077c233d77
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-03-10 17:46:22 -0300

    EsLint 5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import * as index from './pages/index';
+// import * as index from './pages/index';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={ index.MovieList } />
-        <Route path="/movies/new" component={ index.NewMovie } />
-        <Route path="/movies/:id/edit" component={ index.EditMovie } />
-        <Route path="/movies/:id" component={ index.MovieDetails } />
-        <Route path="*" component={ index.NotFound } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </Router>
   );
