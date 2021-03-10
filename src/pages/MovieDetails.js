20e7422d49ca2a10e20edcf34697e3b201c1bdff
cleber-teixeira <cleberlt@gmail.com>
2021-03-10 12:52:06 -0300

    Requisito 01 - Refatoração-4 (eslint). :white_check_mark:

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -42,7 +42,7 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to="/">VOLTAR</Link>
-        <Link to={ `/movies/:${ id }/edit` }>EDITAR</Link>
+        <Link to={ `/movies/:${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
