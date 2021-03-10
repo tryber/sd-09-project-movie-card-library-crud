f8b1c4a32579c6106d33358ce553bfa3f179b1c6
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-03-10 15:54:42 -0300

    EsLint 1

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
-
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, id, imagePath } = movie;
@@ -17,5 +17,13 @@ class MovieCard extends React.Component {
     );
   }
 }
-
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    id: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 export default MovieCard;
