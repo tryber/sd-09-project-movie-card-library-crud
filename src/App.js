7a4a25f18ceed32cbe838a3a9ee511332882356e
ggaldino95 <ggaldino95@gmail.com>
2021-03-10 19:31:05 -0300

    [Task 5] EditMovie now successfuly updates the selected movie when submitted

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
         <Route exact path="/" component={ Movielist } />
