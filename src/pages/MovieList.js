0552033692fde8b480ed3ca0ca0454cb202146e0
palenske <viniciuspalenske@gmail.com>
2021-03-10 13:58:22 -0300

    [FEAT][Movies List Loading]

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
+    this.fetchMovies = this.fetchMovies.bind(this);
     this.state = {
       movies: [],
     };
   }
 
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    const moviesList = await movieAPI.getMovies();
+    this.setState(({ movies }) => ({
+      movies: [...movies, ...moviesList],
+    }));
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    function loadMovieList() {
+      if (movies.length === 0) {
+        return 'Carregando...';
+      }
+      return movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+    }
 
     return (
       <div data-testid="movie-list">
-        <h1> Movie List</h1>
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {console.log(movies)}
+        { loadMovieList() }
       </div>
     );
   }
