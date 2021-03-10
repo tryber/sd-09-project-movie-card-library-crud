a8bb68a3badfbe08eb55f0225afd0969c76b1b93
ggaldino95 <ggaldino95@gmail.com>
2021-03-10 17:38:09 -0300

    [Task 3] MovieCard now has a Link to redirect to a specific movie details by its ID

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = movie;
+    const { id, title, storyline, imagePath } = movie;
 
     return (
       <div className="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
         <div className="movie-card-body">
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <span className="rating">{rating}</span>
         </div>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
