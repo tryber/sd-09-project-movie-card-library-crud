23141165361e702954fad0a792d8ae8adc626c5b
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-03-10 01:52:21 -0300

    Requisito 01

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
     <Router>
       <div>Movie Card Library CRUD</div>
       <Switch>
+        <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
         <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-        <Route path="/" component={ MovieList } />
         <Route component={ NotFound } />
       </Switch>
     </Router>
