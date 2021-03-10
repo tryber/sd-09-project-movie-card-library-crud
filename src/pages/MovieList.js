f32a2dcbfa999daa07a38e2ffa6c66e44a50ae83
ggaldino95 <ggaldino95@gmail.com>
2021-03-10 19:54:00 -0300

    [Task 6] NewMovie successfully fetches the new entry to the movies database and redirects to MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../components/Loading';
 import MovieCard from '../components/MovieCard';
 
@@ -34,6 +35,7 @@ class MovieList extends Component {
 
     return (
       <div data-testid="movie-list">
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
         {movies.length > 0
           ? movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
           : <Loading />}
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = { hasFetched: false };
   }
 
-  handleSubmit(/* newMovie */) {
+  async handleSubmit(newMovie) {
+    try {
+      await movieAPI.createMovie(newMovie);
+      this.setState({ hasFetched: true });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
+    const { hasFetched } = this.state;
+    if (hasFetched) return <Redirect to="/" />;
+
     return (
       <div data-testid="new-movie">
-        <h1>teste</h1>
         <MovieForm onSubmit={ this.handleSubmit } />
       </div>
     );
