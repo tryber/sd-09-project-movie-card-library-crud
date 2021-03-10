f7ed3dba5eaf7c04fee301261dffc53d73d88cc1
Miguel Dantas <miiguelalb@gmail.com>
2021-03-10 11:07:41 -0300

    Req 2 concluído

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       <div>Movie Card Library CRUD</div>
       <switch>
         <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
         <Route component={ NotFound } />
       </switch>
     </Router>
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
+    this.fetchMovies = this.fetchMovies.bind(this);
     this.state = {
       movies: [],
     };
   }
 
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    const movieList = await movieAPI.getMovies();
+    this.setState({ movies: movieList });
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+        {movies.length > 0 ? (
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        ) : (
+          <Loading />
+        )}
       </div>
     );
   }
