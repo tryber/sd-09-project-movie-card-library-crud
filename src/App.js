90421ede9fc4185aea1d1122f80339a0a934ef57
cleber-teixeira <cleberlt@gmail.com>
2021-03-10 12:44:31 -0300

    Requisito 01 - Refatoração-3. :white_check_mark:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
@@ -9,16 +9,17 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       {/* <div>Movie Card Library CRUD</div> */}
       <Switch>
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        {/* <Route path="/movies/:id" component={ MovieDetails } /> */}
         <Route exact path="/" component={ MovieList } />
-        <Route exact path="/movies/new" component={ NewMovie } />
-        <Route exact path="/movies/:id" component={ MovieDetails } />
-        <Route exact path="/movies/:id/edit" component={ EditMovie } />
         <Route component={ NotFound } />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
