7d4d54ea3be52fa86e02a63a50878d96f6897ab1
ggaldino95 <ggaldino95@gmail.com>
2021-03-10 16:50:47 -0300

    [Task 2] MovieList successfully fetched and rendered on initial page

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
+import Loading from '../components/Loading';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
+    this.fetchMovies = this.fetchMovies.bind(this);
 
     this.state = {
       movies: [],
     };
   }
 
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    try {
+      const response = await movieAPI.getMovies();
+      this.setState({
+        movies: response,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    console.log(movies);
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {movies.length > 0
+          ? movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+          : <Loading />}
       </div>
     );
   }
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -15,6 +15,7 @@ class NewMovie extends Component {
   render() {
     return (
       <div data-testid="new-movie">
+        <h1>teste</h1>
         <MovieForm onSubmit={ this.handleSubmit } />
       </div>
     );
