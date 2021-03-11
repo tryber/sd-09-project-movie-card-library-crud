817d6766355fac0c4bb167786f68ffaea2fc9528
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-03-11 00:49:54 -0300

    Requisitos 1 - 4 -

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//  https://reactrouter.com/web/guides/quick-start
+
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,83 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
+// import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
+    this.state = {
+      movie: undefined,
+    };
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.printMovieDetailOrLoading = this.printMovieDetailOrLoading.bind(this);
+    this.printMovieDetails = this.printMovieDetails.bind(this);
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    movieAPI.getMovie(id).then((response) => {
+      this.setState({
+        movie: response,
+      });
+    });
+  }
+
+  printMovieDetailOrLoading() {
+    const { movie } = this.state;
+    if (movie === undefined) {
+      return (
+        <p>Carregando...</p>
+      );
+    }
 
     return (
-      <div data-testid="movie-details">
+      <div>
+        {this.printMovieDetails()}
+      </div>
+    );
+  }
+
+  printMovieDetails() {
+    const { movie } = this.state;
+    const { imagePath, subtitle, storyline, genre, rating, title, id } = movie;
+    return (
+      <div>
+        <p>{ `Title: ${title}` }</p>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+      </div>
+    );
+  }
+
+  render() {
+    // Change the condition to check the state
+    // if (true) return <Loading />;
+
+    return (
+      <div data-testid="movie-details">
+        <p>MovieDetails</p>
+        {this.printMovieDetailOrLoading()}
+        <Link to="/">VOLTAR</Link>
+        <br />
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
