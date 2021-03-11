7238654e0ae3b032f62b2f6d0b27f1702e5bc4e5
Natalia Negreiros <nataliaknegres@gmail.com>
2021-03-10 21:35:50 -0300

    feat/ requirement 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import { EditMovie, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Route path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route path="*" component={ NotFound } />
     </Router>
   );
 }
