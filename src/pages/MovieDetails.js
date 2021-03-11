7388a20d29b4cfadd362850830509ee2deb15415
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-03-10 22:44:43 -0300

    Feat: requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,19 +4,44 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.getMovie = this.getMovie.bind(this);
+    this.state = {
+      movie: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getMovie();
+  }
+
+  async getMovie() {
+    const {match} = this.props;
+    try {
+      this.setState({
+        movie: await movieAPI.getMovie(match.params.id),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    // const { movie } = this.props;
+    // console.log(movie);
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <p>{ `Subtitle: ${subtitle}`}</p>
+        <p>{ `Storyline: ${storyline}`}</p>
+        <p>{ `Genre: ${genre}`}</p>
+        <p>{ `Rating: ${rating}`}</p>
       </div>
     );
   }
--- src/pages/RouteMovies.js
+++ src/pages/RouteMovies.js
@@ -12,10 +12,10 @@ class RouteMovies extends React.Component {
       <main>
         <Switch>
           <Route exact path="/" component={ MovieList } />
-          <Route exact path="/movies/new" component={ NewMovie } />
-          <Route exact path="/movies/:id" component={ MovieDetails } />
-          <Route exact path="/movies/:id/edit" component={ EditMovie } />
-          <Route exact ath="" component={ NotFound } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+          <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+          <Route component={ NotFound } />
         </Switch>
       </main>
     );
