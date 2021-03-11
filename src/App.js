fb61fe8a6ee11cacf2b11198d8da49f53e967943
Pablo Fernandes <pabloprates@gmail.com>
2021-03-11 02:38:58 -0300

    cc war for trybe reasons

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
       <div>Movie Card Library CRUD</div>
       <Switch>
         <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new/" render={ (props) => <NewMovie { ...props } /> } />
         <Route
           exact
           path="/movies/:id/edit/"
           render={ (props) => <EditMovie { ...props } /> }
         />
         <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-        <Route path="/movies/new/" render={ (props) => <NewMovie { ...props } /> } />
         <Route path="*" component={ NotFound } />
       </Switch>
     </Router>
