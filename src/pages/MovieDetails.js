c9ed85f7c109f002f8ee4cdc6626943232d56ca8
Pablo Fernandes <pabloprates@gmail.com>
2021-03-11 11:35:10 -0300

    Adding a function to delete a movie corrected

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
@@ -14,8 +14,11 @@ class MovieDetails extends Component {
       id,
       movie: {},
       isLoading: true,
+      shouldRedirect: false,
     };
     this.getMovieFromAPI = this.getMovieFromAPI.bind(this);
+    this.renderMovieDetails = this.renderMovieDetails.bind(this);
+    this.deleteMovieAPI = this.deleteMovieAPI.bind(this);
   }
 
   componentDidMount() {
@@ -36,20 +39,42 @@ class MovieDetails extends Component {
     );
   }
 
+  deleteMovieAPI() {
+    const { id } = this.state;
+    this.setState(
+      { shouldRedirect: false },
+      () => {
+        movieAPI.deleteMovie(id);
+        this.setState({ shouldRedirect: true });
+      },
+    );
+  }
+
+  renderMovieDetails(movie) {
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    return (
+      <div>
+        <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
+        <p>{ `Subtitle: ${subtitle}` }</p>
+        <p>{ `Storyline: ${storyline}` }</p>
+        <p>{ `Genre: ${genre}` }</p>
+        <p>{ `Rating: ${rating}` }</p>
+      </div>
+    );
+  }
+
   render() {
-    const { isLoading, movie } = this.state;
-    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { isLoading, movie, shouldRedirect } = this.state;
+    const { id } = movie;
+    if (shouldRedirect) { return <Redirect to="/" />; }
     return (
       isLoading ? <Loading />
         : (
           <div data-testid="movie-details">
-            <img alt="Movie Cover" src={ `../${imagePath}` } />
-            <p>{ `Title: ${title}` }</p>
-            <p>{ `Subtitle: ${subtitle}` }</p>
-            <p>{ `Storyline: ${storyline}` }</p>
-            <p>{ `Genre: ${genre}` }</p>
-            <p>{ `Rating: ${rating}` }</p>
+            {this.renderMovieDetails(movie)}
             <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+            <Link to="/" onMouseDown={ this.deleteMovieAPI }>DELETAR</Link>
             <Link to="/">VOLTAR</Link>
           </div>
         )
