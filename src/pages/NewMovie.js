c002ab605a162bcdf7c7ffca8ba69fa4c4c41e7a
palenske <viniciuspalenske@gmail.com>
2021-03-11 12:21:08 -0300

    [FEAT][New Movie]

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      shouldRedirect: false,
+    };
+  }
 
-  // handleSubmit(newMovie) {
-  // }
+  handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie);
+    this.setState({ shouldRedirect: true });
+  }
 
   render() {
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
     return (
       <div data-testid="new-movie">
         <h1> New Movie </h1>
