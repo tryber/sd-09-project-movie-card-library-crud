e6019144e599faa30d829e0aba8d7b823d192dcb
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-11 13:22:36 -0300

    task6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -33,7 +33,7 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-        <Link to="/movies/newMovie">ADICIONAR CARTÃO</Link>
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
