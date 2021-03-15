f460231a4eadbabc33d8dfd3e4fa7b618295fa25
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-15 14:49:07 -0300

    Updates

---
+++ precache-manifest.09e77d7351d0b8cf3afa7a6b8bf224c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3efe464b7919e92915ab332b4aa7ac",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "6231d78f5521172a550e",
+    "url": "/app-toot-name/static/css/main.de3b9055.chunk.css"
+  },
+  {
+    "revision": "0119356adf8415a13f68",
+    "url": "/app-toot-name/static/js/2.c66b20ec.chunk.js"
+  },
+  {
+    "revision": "6231d78f5521172a550e",
+    "url": "/app-toot-name/static/js/main.c4b25529.chunk.js"
+  },
+  {
+    "revision": "10f00aa2016367073b45",
+    "url": "/app-toot-name/static/js/runtime-main.fb5cccab.js"
+  }
+]);
