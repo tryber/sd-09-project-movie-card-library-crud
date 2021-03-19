5801fadc810d19c6ff292d01c5091c84c5c98fdb
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-03-19 16:18:48 -0300

    requisito 1 - rotas no App.js

---
+++ src/components/RouteMovies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import { MovieDetails, MovieList, EditMovie, NotFound, NewMovie } from '../pages';
+
+export default function RouteMovies() {
+  return (
+    <Switch>
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route exact path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/" component={ MovieList } />
+      <Route component={ NotFound } />
+    </Switch>
+  );
+}
