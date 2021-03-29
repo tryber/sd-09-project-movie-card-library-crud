8e7ddc1b96778b35ea7bda10201a5b22761bcb74
Marina Alane <marina.alane@outlook.com>
2021-03-29 09:45:37 -0300

    fix requirement 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -10,7 +10,7 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
-      loading: false,
+      loading: 'loading',
     };
   }
 
@@ -27,10 +27,11 @@ class MovieList extends Component {
     const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-    if (!loading) return <Loading />;
+    if (loading === 'loading') return <Loading />;
     return (
       <div data-testid="movie-list">
         { movies.map((movie) => (<MovieCard key={ movie.title } movie={ movie } />)) }
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      shouldRedirect: false,
+    };
   }
 
-  handleSubmit(/* newMovie */) {
+  handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie).then(() => this.setState({
+      shouldRedirect: true,
+    }));
   }
 
   render() {
+    const { shouldRedirect } = this.state;
+
+    if (shouldRedirect) return (<Redirect to="/" />);
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
       </div>
     );
   }
+  // consultei o repositório do colega Murilo Batista
+  // https://github.com/tryber/sd-09-project-movie-card-library-crud/pull/50/files?file-filters%5B%5D=.js
 }
 export default NewMovie;
