d2f9a95a9276137f1868875819646b9e00fb5ccb
Mathaus <mathausannibal@gmail.com>
2021-03-30 01:59:15 -0300

    adicona req 2 e req3

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +11,34 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.fecthMovie = this.fecthMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fecthMovie();
+  }
+
+  async fecthMovie() {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      movies,
+      loading: false,
+    });
   }
 
   render() {
     const { movies } = this.state;
+    const { loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
