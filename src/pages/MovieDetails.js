56ab5ad8afbd4dfc7995f12919d672bab187da87
Kevin Fraga <kevinciolac@gmail.com>
2021-04-02 19:24:23 -0300

    :three: MovieCard Component

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,19 +4,39 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {},
+      loading: false,
+    };
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+  movieInfo(movie) {
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
-      <div data-testid="movie-details">
+      <div>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h1>{ `Title: ${title}` }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+      </div>
+    );
+  }
+
+  render() {
+    // Change the condition to check the state
+    // if (true) return <Loading />;
+    const { loading, movie } = this.state;
+    return (
+      <div data-testid="movie-details">
+        {
+          loading
+            ? <Loading />
+            : this.movieInfo(movie)
+        }
       </div>
     );
   }
