fbd90cd4e0ecf62935f8030ecc1d4d4a020ec045
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-04-04 16:09:56 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Route exact path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      {/* <Redirect path="" component={} /> */}
     </Router>
   );
 }
