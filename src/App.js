d8adf0a62edb1025fe14544d364510191aa43e0b
Carine Becker <caarine.m@gmail.com>
2021-04-04 21:27:09 -0300

    fixes

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
-        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/" component={ MovieList } />
         <Route path="*" component={ NotFound } />
       </Switch>
