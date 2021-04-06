a5ec744ea42719414ec76e8fbdbaeee58c371e4e
Pedro Amaral <amaralph1995@gmail.com>
2021-04-06 19:59:46 -0300

    Inicia projeto movie CRUD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+    </BrowserRouter>
   );
 }
 
