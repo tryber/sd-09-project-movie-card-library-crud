9fc8c9345950ac2fd7c1733282f6f14d2417f0af
Micaela Alves <micaela.alves93@gmail.com>
2021-04-11 05:42:50 -0300

    Estrutura componente movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, imagePath, rating, storyline, subtitle, title } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        {/* <img src={ imagePath } alt="Imagem do Filme" /> */}
+        <section>
+          <h4>{ title }</h4>
+          {/* <h5>{ subtitle }</h5> */}
+          <p>{ storyline }</p>
+        </section>
+        {/* <div>{ rating }</div> */}
+        <Link to={ `/movies/${id}` }> VER DETALHES </Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
