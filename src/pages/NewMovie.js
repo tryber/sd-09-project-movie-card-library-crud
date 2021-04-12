5708efb60de6b106f85799be3b69c049c5820d87
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-04-11 22:47:33 -0300

    editmovie

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleSubmit(newMovie) {
-  // }
+  handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
+  }
 
   render() {
+    const { shouldRedirect } = this.state;
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
+        { shouldRedirect
+          ? <Redirect to="/" />
+          : <MovieForm onSubmit={ this.handleSubmit } /> }
       </div>
     );
   }
