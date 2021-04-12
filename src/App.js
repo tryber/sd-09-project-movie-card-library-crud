6d2d13b9583e954e3a39464d308baaab7f10687b
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-04-11 22:15:27 -0300

    editmovie

--- src/App.js
+++ src/App.js
@@ -8,19 +8,31 @@ import NotFound from './pages/NotFound';
 import './App.css';
 
 class App extends React.Component {
+  createRoute() {
+    return (
+      <div>
+        <Route exact path="/" component={ MovieList } />
+        <Route
+          exact
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
+        <Route component={ NotFound } />
+      </div>
+    );
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div>Movie Card Library CRUD</div>
         <Switch>
-          <Route exact path="/" component={ MovieList } />
-          <Route exact path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-          <Route path="/movies/new" component={ NewMovie } />
-          <Route
-            path="/movies/:id/edit"
-            render={ (props) => <EditMovie { ...props } /> }
-          />
-          <Route component={ NotFound } />
+          { this.createRoute() }
         </Switch>
       </BrowserRouter>
     );
