c9b1505919a94275d9378fc75edd4bf32e15df88
Micaela Alves <micaela.alves93@gmail.com>
2021-04-14 20:40:52 -0300

    linha duplicada

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
         />
         <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route path="*" component={ NotFound } />
-        <Route path="/movies/new" component={ NewMovie } />
       </Switch>
     </BrowserRouter>
   );
