259cbc018a5b1e67fd2c984c7b9b050c74182dac
Pedro Piquini <piquini.pedro@gmail.com>
2021-04-19 15:37:00 -0300

    Requisito 1 projeto CRUD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <Route exact path="/" component={ MovieList } />
+      <Route exact path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route exact path="" component={ NotFound } />
+    </BrowserRouter>
   );
 }
 
