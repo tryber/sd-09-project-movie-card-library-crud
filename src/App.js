7076d334f0fbbd97d5ec94cacd36b89af9187b5c
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2021-05-02 13:38:18 -0300

    requisito 01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="" component={ NotFound } />
+        {/* https://stackoverflow.com/questions/49162311/react-difference-between-route-exact-path-and-route-path */}
+      </Switch>
     </BrowserRouter>
-    
   );
 }
 
