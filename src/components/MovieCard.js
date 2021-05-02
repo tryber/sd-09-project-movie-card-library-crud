1b2342a6cb9e8a3bc4fd789d03198c97c4f7c91e
jepaur5 <jepaur5@gmail.com>
2021-05-02 16:48:30 -0300

    requisitos jesus corrigindo App-js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,26 +8,24 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { id, title, storyline, imagePath } = movie;
     return (
-      <div className="movie-card" data-testid="movie-card">
-        <article>
-          <div>
-            <img src={ imagePath } alt="Movie" />
-          </div>
-          <p>{title}</p>
-          <p>{storyline}</p>
-          <hr />
-          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
-        </article>
+      <div data-testid="movie-card">
+        Movie Card
+        <img src={ imagePath } alt="Movie Cover" />
+        <h1>{ title }</h1>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
 
 MovieCard.propTypes = {
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-}.isRequired;
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
