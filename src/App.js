db8d40db3a3d9f954e0f816365d0d0364014ac52
jepaur5 <jepaur5@gmail.com>
2021-05-02 17:24:39 -0300

    requisito add prop-types

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
-import { Route, Switch } from 'react-router';
-import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/movies/new" component={ NewMovie } />
-        <Route exact path="/movies/:id" component={ MovieDetails } />
-        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/" component={ MovieList } />
         <Route component={ NotFound } />
       </Switch>
-      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
     </BrowserRouter>
   );
 }
