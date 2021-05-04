bf01ac72e1d19e74619812812f5b407a6a83cd16
Pedro Amaral <amaralph1995@gmail.com>
2021-05-04 01:18:48 -0300

    Corrige lint de EditMovie

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
-      <h1>Carregando...</h1>
+      <div>Carregando...</div>
     );
   }
 }
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import  { Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -22,21 +23,21 @@ class EditMovie extends Component {
       this.setState({
         status: false,
         movie: response,
-      })
-    })
+      });
+    });
   }
 
   handleSubmit(updatedMovie) {
-    movieAPI.updateMovie(updatedMovie)
+    movieAPI.updateMovie(updatedMovie);
     this.setState({
       shouldRedirect: true,
-    })
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      return <Redirect to="/" />
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
@@ -50,5 +51,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
 
 export default EditMovie;
