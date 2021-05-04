739da704e2d64d4faa2e6f3f136de0f8f13173d3
Pedro Amaral <amaralph1995@gmail.com>
2021-05-04 02:11:50 -0300

    Corrige lint de MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -5,8 +5,6 @@ import { getMovies } from '../services/movieAPI';
 import MovieCard from '../components/MovieCard';
 import { Loading } from '../components';
 
-import * as movieAPI from '../services/movieAPI';
-
 class MovieList extends Component {
   constructor() {
     super();
@@ -22,28 +20,23 @@ class MovieList extends Component {
       this.setState({
         movies: response,
         loading: false,
-      })
-    })
+      });
+    });
   }
 
   render() {
     const { movies, loading } = this.state;
-
-    // Render Loading here if the request is still happening
     if (loading) {
-      return <Loading />
+      return <Loading />;
     }
     return (
       <div>
-        <div
-          data-testid="movie-list"
-          className="movie-list"
-        >
+        <div data-testid="movie-list" className="movie-list">
           {movies.map((movie, index) => (
             <MovieCard
               key={ movie.title }
               movie={ movie }
-              index={ index } 
+              index={ index }
             />))}
         </div>
         <button type="button"><Link to="/movies/new">ADICIONAR CARTÃO</Link></button>
