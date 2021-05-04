d0b35ea9919484c2ffdab03ac66332d1ac84bdb8
Pedro Amaral <amaralph1995@gmail.com>
2021-05-04 01:05:41 -0300

    Corrige lint pra MovieCards.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,23 +4,30 @@ import '../App.css';
 import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
-  render(match) {
-    const { movie: { imagePath, id, rating, storyline, title, subtitle }, index } = this.props;
+  render() {
+    const {
+      movie: { imagePath, storyline, title, subtitle },
+      index,
+    } = this.props;
     return (
-      <div
-        data-testid="movie-card"
-        className="movie-card"
-      >
-        <img src={ imagePath } className="card-image" />
+      <div data-testid="movie-card" className="movie-card">
+        <img src={ imagePath } className="card-image" alt="poster do filme" />
         <h2>{ title }</h2>
         <h3>{ subtitle }</h3>
         <p>{ storyline }</p>
-        <Link to={ `/movies/${ index + 1 }` }>
-          <a href={ 'http://localhost/movies/' + (index + 1) }>VER DETALHES</a>
-        </Link>
+        <button type="button">
+          <Link to={ `/movies/${index + 1}` }>
+            VER DETALHES
+          </Link>
+        </button>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.object,
+  index: PropTypes.string,
+}.isRequired;
 
 export default MovieCard;
