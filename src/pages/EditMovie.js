12206c341c4e2147c15bb6b226badc8c8b8a0fc6
Pedro Amaral <amaralph1995@gmail.com>
2021-05-04 00:40:29 -0300

    Cria a função de edição de filmes

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
+import  { Redirect } from 'react-router-dom';
 
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: [],
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    movieAPI.getMovie(id).then((response) => {
+      this.setState({
+        status: false,
+        movie: response,
+      })
+    })
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie)
+    this.setState({
+      shouldRedirect: true,
+    })
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
