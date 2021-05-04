177632991253cc83681f7013f701066b11d3510c
Jean Silveira <jjsilveira007@gmail.com>
2021-05-04 13:11:02 -0300

    :rocket: Para o infinito e além...

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
-import NewMovie from './pages/NewMovie';
-import EditMovie from './pages/EditMovie';
-import NotFound from './pages/NotFound';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
-      <Route path="movies/new" component={ NewMovie } />
-      <Route path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="*" component={ NotFound } />
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route component={ NotFound } />
+      </Switch>
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
     </Router>
   );
 }
