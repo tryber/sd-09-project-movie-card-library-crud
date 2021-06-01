a5224d8adb3d5b5fce05a5e63a41d03901caebb9
FredericoCanoeira <fcanoeira@gmail.com>
2021-06-01 14:07:13 +0100

    REQ1 Render BrowserRouter in the App component using routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />  
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
-
-export default App;
